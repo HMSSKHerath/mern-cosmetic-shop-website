85fe32c2018f8709f3e45475ec3ef908df2eb763
HMSSKHerath <hmsskherath20020903@gmail.com>
2026-06-10 20:02:35 +0530

    Add GET and POST request handlers to log incoming requests

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,16 @@ import express from "express";
 
 const app = express();
 
+app.get("/",() =>
+{
+    console.log("Get request received");
+});
+
+app.post("/",() =>
+{
+    console.log("Post request received");
+});
+
 app.listen(3000, () =>
 {
     console.log("Server is running on port 3000...");
