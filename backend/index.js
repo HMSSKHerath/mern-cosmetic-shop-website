43b470e14b00bc5c1ee8f89458a3fa2eb4a99b48
HMSSKHerath <hmsskherath20020903@gmail.com>
2026-06-10 19:16:08 +0530

    Update server start log message in index.js

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,8 @@ import express from "express";
 
 const app = express();
 
-function serverStart()
+app.listen(3000, () =>
 {
-    console.log("Server is starting...");
-}
-app.listen(3000, serverStart);
+    console.log("Server is running on port 3000...");
+});
 
