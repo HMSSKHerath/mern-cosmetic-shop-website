c89e32c3d7834b60c6dbf0cc9244292764956152
HMSSKHerath <hmsskherath20020903@gmail.com>
2026-06-11 06:09:45 +0530

    Add DELETE and PUT request handlers to log incoming requests

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,16 @@ app.post("/",() =>
     console.log("Post request received");
 });
 
+app.delete("/", () =>
+{
+    console.log("Delete request received");
+});
+
+app.put("/", () =>
+{
+    console.log("Put request received");
+});
+
 app.listen(5000, () =>
 {
     console.log("Server is running on port 5000...");
