4ba74785ffbfc83444eacc75c0b118d1ba1ce261
HMSSKHerath <hmsskherath20020903@gmail.com>
2026-06-27 02:54:30 +0530

    Add createUser function to handle user registration

---
+++ backend/controllers/userController.js
@@ -0,0 +1,23 @@
+import User from "../models/userModel.js";
+
+function createUser(req, res)
+{
+    const newUser = new User(
+    {
+        email: req.body.email,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        password: req.body.password,
+        role: req.body.role,
+    });
+    
+    newUser.save()
+    .then(()=>
+    {
+        res.json({ message: "User created successfully" });
+    })
+    .catch(()=>
+    {
+        res.json({ message: "Error creating user" });
+    });
+}
