94bdf2456ff755869b20e4b09d6221dbca78c2f3
HMSSKHerath <hmsskherath20020903@gmail.com>
2026-06-30 15:31:37 +0530

    Add product model schema for MongoDB integration

---
+++ backend/models/productModel.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+{
+    id : { type: String, required: true, unique: true },
+    name : { type: String, required: true },
+    altNames : { type : [String] , default : [] },
+    description : { type: String, required: true },
+    images : { type : [String] , default : [] , required: true },
+    price : { type: Number, required: true },
+    labelPrice : { type: Number, required: true },
+    category : { type: String, required: true }
+})
+
+const Product = mongoose.model("Product" , productSchema);
+
+export default Product;
