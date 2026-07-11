2a51f4b4ead300bd09d61912dedded624f9b25bb
HMSSKHerath <hmsskherath20020903@gmail.com>
2026-07-11 21:49:07 +0530

    Add dotenv dependency and update server port to use environment variable

--- backend/index.js
+++ backend/index.js
@@ -56,7 +56,7 @@ app.use("/students", studentRouter);
 app.use("/products", productRouter);
 
 // Start the server
-app.listen(5000, () =>
+app.listen(process.env.PORT, () =>
 {
-    console.log("Server is running on port 5000...");
+    console.log(`Server is running on port ${process.env.PORT}...`);
 });
