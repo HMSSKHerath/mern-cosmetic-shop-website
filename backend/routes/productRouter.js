82bed756ba283991b25f88a34835c9d69c984c9f
HMSSKHerath <hmsskherath20020903@gmail.com>
2026-07-11 22:35:51 +0530

    Add authentication and admin role verification to product routes

--- backend/routes/productRouter.js
+++ backend/routes/productRouter.js
@@ -1,12 +1,15 @@
 import express from "express";
+import authMiddleware from "../middlewares/authMiddleware.js";
+import verifyAdminRole from "../middlewares/roleMiddleware.js";
 import {getAllProducts , getProductById , createProduct , deleteProduct , updateProduct} from "../controllers/productController.js";
 
 const productRouter = express.Router();
 
 productRouter.get("/", getAllProducts);
-productRouter.post("/", createProduct);
 productRouter.get("/:productId", getProductById);
-productRouter.delete("/:productId", deleteProduct);
-productRouter.put("/:productId", updateProduct);
+
+productRouter.post("/", authMiddleware, verifyAdminRole, createProduct);
+productRouter.delete("/:productId", authMiddleware, verifyAdminRole, deleteProduct);
+productRouter.put("/:productId", authMiddleware, verifyAdminRole, updateProduct);
 
 export default productRouter; 
