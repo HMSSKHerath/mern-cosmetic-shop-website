b0eee8c445d663a251cb53a3a638cc3ac4fcfe91
HMSSKHerath <hmsskherath20020903@gmail.com>
2026-07-14 20:57:28 +0530

    Initialize backend structure with server, app, and database connection files

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+import express from "express";
+import userRoutes from "./routes/userRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/users", userRoutes);
+app.use("/products", productRoutes);
+
+
+export default app;
