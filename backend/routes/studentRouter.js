060b54418232039fd96fa7a28531af75299fb82c
HMSSKHerath <hmsskherath20020903@gmail.com>
2026-07-14 14:05:27 +0530

    Add authentication and role-based access control to student routes

--- backend/routes/studentRouter.js
+++ backend/routes/studentRouter.js
@@ -1,9 +1,11 @@
 import express from "express";
+import authMiddleware from "../middlewares/authMiddleware.js";
+import roleMiddleware from "../middlewares/roleMiddleware.js";
 import { getStudent , createStudent } from "../controllers/studentController.js";
 
 const studentRouter = express.Router();
 
-studentRouter.get("/", getStudent);
-studentRouter.post("/", createStudent);
+studentRouter.get("/", authMiddleware, roleMiddleware, getStudent);
+studentRouter.post("/", authMiddleware, roleMiddleware, createStudent);
 
 export default studentRouter;
